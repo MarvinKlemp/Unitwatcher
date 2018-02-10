9129aebdac36466cbd0ffc3f369c01bcd241c63b
MarvinKlemp <marvinklemp@sensiolabs.de>
2018-02-10 11:10:40 +0100

    Should listen on test dir until the mapping from src/ to test/ is implemented

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 extern crate notify;
 
-use notify::{Watcher, RecursiveMode, watcher};
+use notify::{Watcher, RecursiveMode, RawEvent, raw_watcher};
 use std::process::{Command, Stdio};
 use std::sync::mpsc::channel;
 use std::time::Duration;
@@ -9,24 +9,23 @@ use std::env;
 fn main() {
     let args: Vec<String> = env::args().collect();
     let project_path = &args[1];
-    let project_src = format!("{}/src", &project_path);
-    let executable = format!("{}/vendor/bin/phpunit", &project_path);
+    let watch_path = format!("{}/tests", &project_path);
+    let executable_path = format!("{}/vendor/bin/phpunit", &project_path);
 
     let (tx, rx) = channel();
-    let mut watcher = watcher(tx, Duration::from_secs(2)).unwrap();
+    let mut watcher = raw_watcher(tx).unwrap();
 
-    watcher.watch(&project_src, RecursiveMode::Recursive).unwrap();
+    watcher.watch(&watch_path, RecursiveMode::Recursive).unwrap();
 
-    println!("watching {:?}", &
-        project_src);
+    println!("watching {:?}", &watch_path);
     loop {
         match rx.recv() {
-            Ok(event) => {
-                println!("File changed: running executable: '{:?}'", &executable);
-                println!();
-
-                let mut command = Command::new(&executable)
-                    .args(&[format!("{}/tests", &project_path)])
+            Ok(RawEvent{path: Some(path), op: Ok(op), cookie}) => {
+                let mut command = Command::new(&executable_path)
+                    .args(&[
+                        &watch_path,
+                        &format!("--filter={:?}", &path.file_stem().unwrap())
+                    ])
                     .stdout(Stdio::inherit())
                     .stderr(Stdio::inherit())
                     .spawn()
@@ -34,11 +33,8 @@ fn main() {
 
                 let status = command.wait();
             },
+            Ok(event) => println!("broken event: {:?}", event),
             Err(e) => println!("watch error:{:?}", e),
         }
     }
-}
-
-fn execute() {
-
 }
